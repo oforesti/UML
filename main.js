cc18a124e54254a2247a8e1373663dc44890987f
Pedro Coltro 👾 <138863769+coltrox@users.noreply.github.com>
2025-08-16 23:37:56 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const path = require('path');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+    icon: path.join(__dirname, 'public', 'logo.png'),
+    show: false
+  });
+
+  // Sempre abre na tela inicial (index.html)
+  mainWindow.loadFile(path.join(__dirname, 'public', 'index.html'));
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+ipcMain.handle('select-folder', async () => {
+  const result = await dialog.showOpenDialog({ properties: ['openDirectory'] });
+  return result.filePaths[0];
+});
